9f838d4de1a0113ff1dd49a2ad651a979a715061
kmaiap <kaillany.proenca@aluno.senai.br>
2026-03-05 08:18:14 -0300

    first commit

---
+++ springboot/rh/src/main/java/com/example/rh/Repository/FuncionarioRepository.java
@@ -0,0 +1,21 @@
+package com.example.rh.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.rh.Model.Funcionario;
+import java.util.List;
+
+
+public interface FuncionarioRepository extends CrudRepository<Funcionario, Long> {
+    // métodos para realizar o crud do Funcionario
+    //criar uma busca pela chave primaria do Funcionario
+    Funcionario findById (long id);
+
+    //busca pelo nome
+    Funcionario findByNome (String nome);
+
+    //Busca para Vários Nomes // não existe no JPA
+    @Query(value =  "select u from Funcionario u where u.nome like %?1%")
+    List<Funcionario> findByNomeContaining(String nome);
+}
---
+++ springboot/teste_thymeleaf/src/main/java/com/example/teste_thymeleaf/TesteThymeleafApplication.java
@@ -0,0 +1,14 @@
+package com.example.teste_thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteThymeleafApplication.class, args);
+	}
+
+}
+
